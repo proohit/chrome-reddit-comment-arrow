92db47b3917e33d0edd0a7ff1d2b50690779624f
proohit <direnc99@gmail.com>
2022-09-08 16:47:58 +0200

    feat: added build pipeline
    feat: add automatic release tag to pipeline
    
    feat: added license
    
    fix: pipeline
    
    feat: added automatic tagging
    
    feat: fixed tag prefix
    
    fix: add sub directories in zip

---
+++ set_version.py
@@ -0,0 +1,8 @@
+import os
+import json
+
+env_file_path = os.getenv('GITHUB_ENV')
+
+with open(env_file_path, "a") as env_file:
+    version = json.load(open('manifest.json'))['version']
+    env_file.write(f"VERSION={version}")
