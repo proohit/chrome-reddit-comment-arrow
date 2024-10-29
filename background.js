a115d3bca9a19e28839c1d60c27b61f3de6f13e8
proohit <direnc99@gmail.com>
2024-10-29 14:59:45 +0100

    feat: adds support for options from context menu

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "reddit-comment-arrow-settings") {
+    await chrome.runtime.openOptionsPage();
+  }
+});
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create({
+  id: "reddit-comment-arrow-settings",
+  title: "Reddit Comment Arrow",
+  documentUrlPatterns: ["https://www.reddit.com/*", "https://reddit.com/*"],
+});
